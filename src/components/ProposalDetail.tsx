c92406d8a7d1f840d26eaf43c5d287c8dabd440f
Andrei Shestakov <75143257+shestakovar@users.noreply.github.com>
2021-09-30 22:26:34 +0400

    Add some styles to proposal detail

--- src/components/ProposalDetail.tsx
+++ src/components/ProposalDetail.tsx
@@ -2,59 +2,77 @@ import React, { FC } from 'react';
 import { IProposal } from "../types/types";
 import { convertCurrency, convertTime } from "../utils/print";
 import MyButton from "./UI/MyButton/MyButton";
+import classes from './ProposalDetail.module.css'
+import { useHistory } from "react-router-dom";
 
 interface ProposalDetailProps {
   proposal: IProposal
 }
 
 const ProposalDetail: FC<ProposalDetailProps> = ({ proposal }) => {
+  const history = useHistory();
   return (
-    <div>
-      <h1>Название предложения: {proposal.name}</h1>
-      <h2>Банк: {proposal.organization.name}. Номер лицензии: {proposal.organization.license}</h2>
+    <>
+      <MyButton text='Назад' onClick={history.goBack}/>
+      <div className={classes.table_wrapper}>
+        <table className={classes.table}>
+          <tbody>
+          <tr>
+            <td>Название предложения</td>
+            <td>{proposal.name}</td>
+          </tr>
+          <tr>
+            <td>Банк</td>
+            <td>{proposal.organization.name}</td>
+          </tr>
+          <tr>
+            <td>Номер лицензии</td>
+            <td>{proposal.organization.license}</td>
+          </tr>
+          <tr>
+            <td>Сумма</td>
+            <td>{proposal.rate.creditAmount?.to
+              ? `${proposal.rate.creditAmount.from.toLocaleString()}  ${convertCurrency(proposal.rate.currency)} – ${proposal.rate.creditAmount.to.toLocaleString()} ${convertCurrency(proposal.rate.currency)}`
+              : `от ${proposal.rate.creditAmount.from.toLocaleString()} ${convertCurrency(proposal.rate.currency)}`
+            }</td>
+          </tr>
 
-      <div>
-        <h2>Требования к получателю кредита:</h2>
-        <div>Возраст: {proposal.customerRequirements.age}</div>
-        <div>Количество документов: {proposal.customerRequirements.documents}</div>
-        <div>Возраст(М): {proposal.customerRequirements.manAgeAtRepayment}</div>
-        <div>Возраст(Ж): {proposal.customerRequirements.femaleAgeAtRepayment}</div>
+          <tr>
+            <td>Процент</td>
+            <td>{proposal.rate.periods[0].rate.from === proposal.rate.periods[0].rate.to
+              ? proposal.rate.periods[0].rate.from + '%'
+              : 'от ' + proposal.rate.periods[0].rate.from + '%'
+            }</td>
+          </tr>
+
+          <tr>
+            <td>Срок</td>
+            <td>От {convertTime(proposal.rate.periods[0].term.from, proposal.rate.periods[0].termUnit)} до {convertTime(proposal.rate.periods[0].term.to, proposal.rate.periods[0].termUnit)}</td>
+          </tr>
+
+
+          <tr className={classes.centered}>
+            <td colSpan={2} className={classes.heading}>Требования к получателю кредита</td>
+          </tr>
+          <tr>
+            <td>Возраст</td>
+            <td>{proposal.customerRequirements.age}</td>
+          </tr>
+          <tr>
+            <td>Количество документов</td>
+            <td>{proposal.customerRequirements.documents}</td>
+          </tr>
+          </tbody>
+          <tfoot>
+          <tr>
+            <td colSpan={2}><MyButton text='Перейти на сайт' className={classes.button}/></td>
+          </tr>
+
+          </tfoot>
+        </table>
+
       </div>
-
-      <div>
-        <h2>Сумма: </h2>
-        <span>
-          {proposal.rate.creditAmount?.to
-            ? `${proposal.rate.creditAmount.from.toLocaleString()}  ${convertCurrency(proposal.rate.currency)} – ${proposal.rate.creditAmount.to.toLocaleString()} ${convertCurrency(proposal.rate.currency)}`
-            : `от ${proposal.rate.creditAmount.from.toLocaleString()} ${convertCurrency(proposal.rate.currency)}`
-          }
-        </span>
-      </div>
-
-      <div>
-        <h2>Процент:</h2>
-        <span>
-          {proposal.rate.periods[0].rate.from === proposal.rate.periods[0].rate.to
-            ? proposal.rate.periods[0].rate.from + '%'
-            : 'от ' + proposal.rate.periods[0].rate.from + '%'
-          }
-        </span>
-      </div>
-
-      <div>
-        <h2>Срок: </h2>
-        <span>
-          От {convertTime(proposal.rate.periods[0].term.from, proposal.rate.periods[0].termUnit)} до {convertTime(proposal.rate.periods[0].term.to, proposal.rate.periods[0].termUnit)}
-        </span>
-      </div>
-
-      <div>
-        <MyButton text='Перейти на сайт' />
-      </div>
-
-
-
-    </div>
+    </>
   );
 };
 
